299f79c7155bd8f9afaec450aacd899a3fb930fb
Gnaohmai1511 <gnaohmai1511@gmail.com>
2026-01-29 15:30:33 +0700

    cart routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
@@ -10,13 +11,14 @@ import usersRoutes from "./routes/user.route.js";
 import ordersRoutes from "./routes/order.route.js";
 import reviewsRoutes from "./routes/review.route.js";
 import productsRoutes from "./routes/product.route.js";
-
+import cartRoutes from "./routes/cart.route.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware());// request authentication middleware
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
@@ -25,6 +27,7 @@ app.use("/api/users",usersRoutes);// Users routes
 app.use("/api/orders",ordersRoutes);
 app.use("/api/reviews",reviewsRoutes);
 app.use("/api/products",productsRoutes);
+app.use("/api/cart",cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "OK" });
