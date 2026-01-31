083d9f80b88ed0bcadec74cd5a29b63b4d348574
Gnaohmai1511 <gnaohmai1511@gmail.com>
2026-01-31 18:18:22 +0700

    dashboard page added

--- admin/src/components/PageLoader.jsx
+++ admin/src/components/PageLoader.jsx
@@ -2,8 +2,8 @@ import { LoaderIcon } from "lucide-react";
 
 function PageLoader() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <LoaderIcon className="size-12 animate-spin" />
+    <div className="flex items-center justify-center h-screen bg-base-100 ">
+      <LoaderIcon className="size-12 animate-spin text-primary" />
     </div>
   );
 }
--- admin/src/layouts/DashboardLayout.jsx
+++ admin/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+import { NAVIGATION } from "../components/Navbar.jsx";
+
 
 function DashboardLayout() {
   return (
-  <div>
-    <h1>sidebar</h1>
-    <h2>navbar</h2>
-    <Outlet />
+  <div className="drawer lg:drawer-open">
+    <input id="my-drawer" type="checkbox" className="drawer-toggle" defaultChecked />
+    <div className="drawer-content">
+      <Navbar />
+      <main className="p-6">
+
+  
+      <Outlet />
+      </main>
+    </div>
+    <Sidebar />
   </div>
 
   );
